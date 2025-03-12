021d92ad864bcab3cb218d15555e10d99c2ba9f6
KyoungMin HEO <stalvede0421@gmail.com>
2025-03-12 23:00:29 +0900

    first commit

---
+++ src/main/java/com/kks/shop/KksApplication.java
@@ -0,0 +1,13 @@
+package com.kks.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kks/shop/KksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kks.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
