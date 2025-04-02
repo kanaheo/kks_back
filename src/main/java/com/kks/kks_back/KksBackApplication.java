574e170c5c62f94268c8596686415d441253f2a2
KyoungMin HEO <stalvede0421@gmail.com>
2025-04-02 11:02:40 +0900

    init

---
+++ src/main/java/com/kks/kks_back/KksBackApplication.java
@@ -0,0 +1,13 @@
+package com.kks.kks_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KksBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KksBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kks/kks_back/KksBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kks.kks_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KksBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
